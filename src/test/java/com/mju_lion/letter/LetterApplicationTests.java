75ac69b42c54287856000ba4d20cf23668f67283
BonSeung <bs.koo@sqisoft.com>
2024-06-15 16:56:11 +0900

    first commit

---
+++ src/test/java/com/mju_lion/letter/LetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mju_lion.letter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
