574d1d0c2a974e4cf3eb9760d414568681435dce
BonSeung <bs.koo@sqisoft.com>
2024-06-19 02:40:08 +0900

    docs:엔티티 수정 -#7

--- src/main/java/com/mju_lion/letter/entity/UserTerm.java
+++ src/main/java/com/mju_lion/letter/entity/UserTerm.java
@@ -1,9 +1,6 @@
 package com.mju_lion.letter.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -21,4 +18,7 @@ public class UserTerm extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "term_id")
     private Term term;
+
+    @Column(nullable = false)
+    private boolean agreed;
 }
