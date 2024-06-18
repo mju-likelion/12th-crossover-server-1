c7d73e5fe9b612627dc643657fe52eff50f500de
juryheed <rabitangl20@naver.com>
2024-06-18 20:28:30 +0900

    comment: 주석정리 - #8

--- src/main/java/com/mju_lion/letter/error/ErrorCode.java
+++ src/main/java/com/mju_lion/letter/error/ErrorCode.java
@@ -8,23 +8,23 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    //인증
+    //UnauthorizedException
     PASSWORD_NOT_EQUAL("4010", "비밀번호를 잘못 입력하셨습니다. 다시 입력해 주세요."),
 
-    //리소스 중복
-    USERID_DUPLICATION("4090", "사용할수 없는 아이디 입니다"),
-
-    //인가
+    //ForbiddenExeption
     TOKEN_NOT_FOUND("4031", "토큰이 없습니다"),
     TOKEN_INVALID("4032", "토큰이 유효하지 않습니다"),
-    PASSWORD_("4032", "토큰이 유효하지 않습니다"),
-    //404
-    USER_NOT_FOUND("4041",""),
-    USERID_NOT_FOUND("4041","잘못된 아이디를 입력하셨습니다 다시 입력해 주세요 "),
+
+    //NotFoundException
+    USER_NOT_FOUND("4040","유저가 존재하지 않습니다"),
+    USERID_NOT_FOUND("4041","아이디를 잘못 입력하셨습니다. 다시 입력해 주세요 "),
+
+    //ConflictException
+    USERID__ALREADY_EXISTS("4090", "사용할수 없는 아이디 입니다"),
 
     NOT_NULL("9001", "필수값이 누락되었습니다."),
     NOT_BLANK("9002", "필수값이 빈 값이거나 공백으로 되어있습니다."),
-    REGEX("9003", "형식에 맞지 않습니다."),
+    REGEX("9003", "이메일 형식에 맞지 않습니다."),
     LENGTH("9004", "길이가 유효하지 않습니다.");
 
 
