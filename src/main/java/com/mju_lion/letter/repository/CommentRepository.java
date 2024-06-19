69851861f1ccab8434c8c39966517a03b1242b98
BonSeung <bs.koo@sqisoft.com>
2024-06-20 01:42:37 +0900

    feat:CommentRepository 추가 -#13

---
+++ src/main/java/com/mju_lion/letter/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.mju_lion.letter.repository;
+
+import com.mju_lion.letter.entity.Board;
+import com.mju_lion.letter.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findByBoardId(UUID boardId);
+    Page<Comment> findByBoard(Board board, Pageable pageable);
+}
