1244ff91227584946018e3406592a8b8da9d4883
BonSeung <bs.koo@sqisoft.com>
2024-06-20 18:27:00 +0900

    feat: 약관 불러오는 api 추가 - #15

---
+++ src/main/java/com/mju_lion/letter/controller/TermController.java
@@ -0,0 +1,29 @@
+package com.mju_lion.letter.controller;
+
+import com.mju_lion.letter.dto.response.ResponseDto;
+import com.mju_lion.letter.dto.response.term.TermListResponseData;
+import com.mju_lion.letter.dto.response.term.TermResponseData;
+import com.mju_lion.letter.service.TermService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/terms")
+public class TermController {
+
+    private final TermService termService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDto> getAllTerms() {
+        List<TermResponseData> termResponseDataList = termService.getAllTerms();
+        TermListResponseData termList = new TermListResponseData(termResponseDataList);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "약관 전체 조회 완료", termList), HttpStatus.OK);
+    }
+}
