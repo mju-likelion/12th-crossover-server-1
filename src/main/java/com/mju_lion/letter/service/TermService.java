345c7f61e7f902076ca17e7c8a640e300de6c3f7
BonSeung <bs.koo@sqisoft.com>
2024-06-20 18:28:05 +0900

    feat: 약관 전체 조회 메서드 추가 - #15

---
+++ src/main/java/com/mju_lion/letter/service/TermService.java
@@ -0,0 +1,29 @@
+package com.mju_lion.letter.service;
+
+import com.mju_lion.letter.dto.response.term.TermResponseData;
+import com.mju_lion.letter.entity.Term;
+import com.mju_lion.letter.repository.TermRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TermService {
+
+    private final TermRepository termRepository;
+
+    /**
+     * 약관 전체 조회
+     */
+    public List<TermResponseData> getAllTerms() {
+        List<TermResponseData> termResponseDataList = termRepository.findAll().stream()
+                .map(term -> new TermResponseData(term))
+                .collect(Collectors.toList());
+
+        return termResponseDataList;
+    }
+}
