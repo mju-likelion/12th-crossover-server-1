2d7c5e0cb995e11a12ebfbc871a5fda028e72a03
BonSeung <bs.koo@sqisoft.com>
2024-06-21 15:12:51 +0900

    feat: 토큰 담을 dto 생성 - #16

---
+++ src/main/java/com/mju_lion/letter/dto/response/token/TokenResponseDto.java
@@ -0,0 +1,10 @@
+package com.mju_lion.letter.dto.response.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponseDto {
+    private String AccessToken;
+}
