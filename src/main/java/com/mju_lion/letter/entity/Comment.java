31f3ecbc90e6e1ae6f4bb3ab9444bc4b9ad5b7a8
BonSeung <bs.koo@sqisoft.com>
2024-06-23 23:19:54 +0900

    refactor: 댓글 검증 메서드 추가 - #35 - 로그인한 사용자 id와 댓글 작성자 id 비교 - 게시물에 작성된 댓글 id와 삭제하려는 댓글 id 비교

--- src/main/java/com/mju_lion/letter/entity/Comment.java
+++ src/main/java/com/mju_lion/letter/entity/Comment.java
@@ -1,5 +1,7 @@
 package com.mju_lion.letter.entity;
 
+import com.mju_lion.letter.exception.ForbiddenException;
+import com.mju_lion.letter.exception.errorcode.ErrorCode;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,7 +12,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment extends BaseEntity {
-    @Column(length = 1000,nullable = false)
+    @Column(length = 1000, nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -21,4 +23,15 @@ public class Comment extends BaseEntity {
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
+    public void validateCommentByUserId(Comment comment, User user) {
+        if (!comment.getUser().getId().equals(user.getId())) {
+            throw new ForbiddenException(ErrorCode.COMMENT_NOT_MATCH);
+        }
+    }
+
+    public void validateCommentByBoardId(Comment comment, Board board) {
+        if (!comment.getBoard().getId().equals(board.getId())) {
+            throw new ForbiddenException(ErrorCode.COMMENT_NOT_BELONG_TO_BOARD);
+        }
+    }
 }
